bc1d4bf12d2f28d5f17ec8d3be0c66fe5a99317c
Vo Thanh Trang <137174021+ThanhTrangVo@users.noreply.github.com>
2024-05-05 23:06:40 +0700

    Update bai2.js

--- bai2.js
+++ bai2.js
@@ -1,67 +1,55 @@
+var images = document.querySelectorAll('.image img')
+var iconClose = document.querySelector('.close')
+var btnPrev = document.querySelector('.prev')
+var btnNext = document.querySelector('.next')
+var galleryImg = document.querySelector('.gallery-inner img')
+var gallery = document.querySelector('.gallery')
 
-var images =  document.querySelectorAll('.image img');
-var prev = document.querySelector('.prev');
-var next = document.querySelector('.next');
-var close = document.querySelector('.close');
-var galleryImg = document.querySelector('.gallery_inner img');
-var gallery = document.querySelector('.gallery');
 var currentIndex = 0;
 
-
-
-
-
 function showGallery() {
-
-    if (currentIndex == 0) {
-        gallery.classList.add('show');
+    // display gallery
+    if(currentIndex == 0) {
+        btnPrev.classList.add('hide')
+    } else {
+        btnPrev.classList.remove('hide')
     }
-    else{
-        gallery.classList.remove('show');
-
+    if(currentIndex == images.length - 1) {
+        btnNext.classList.add('hide')
+    } else {
+        btnNext.classList.remove('hide')
     }
-    if (currentIndex == images.length -1) {
-        gallery.classList.add('show');
-    }
-    else{
-        gallery.classList.remove('show');
-    }
+    galleryImg.src = images[currentIndex].src
+    gallery.classList.add('show')
 }
+
 images.forEach((item, index) => {
     item.addEventListener('click', function() {
-      currentIndex = index;
-      showGallery();
-    });
-  });
+        currentIndex = index
+        showGallery()
+    })
+})
 
-//khi user click nút X
-close.addEventListener('click', function() {
+iconClose.addEventListener('click', function() {
     gallery.classList.remove('show')
 })
 
-document.addEventListener('keydown', function(){
-    if (e.keyCode ==27) {
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode == 27) {
         gallery.classList.remove('show')
     }
 })
 
-
-
-//click nút prev
-prev.addEventListener('click', function () { 
-    if (currentIndex > 0) {
-        currentIndex--;
-        showGallery();
-    };
-});
-//click nút next
-next.addEventListener('click', function() {
-    if (currentIndex < images.length -1) {
-        currentIndex++;
-        showGallery();
+btnPrev.addEventListener('click', function() {
+    if(currentIndex > 0) {
+        currentIndex--
+        showGallery()
     }
-});
+})
 
-
-
-
+btnNext.addEventListener('click', function() {
+    if(currentIndex < images.length - 1) {
+        currentIndex++
+        showGallery()
+    }
+})
